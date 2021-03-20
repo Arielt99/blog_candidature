de962ed3a0f555d7c3376560364170be4820d089
Ariel <arielthibault@yahoo.fr>
2021-03-20 02:32:56 +0100

    so sleepy

--- database/migrations/2021_03_16_171440_create_posts_table.php
+++ database/migrations/2021_03_16_171440_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->unsigned();
+            $table->integer('user_id')->default(1);
             $table->string('title');
             $table->text('body');
             $table->timestamps();
